c46a32c9ad1753607ba6a6ebd60c1e7a39afd8f7
ninja1mmm <matx@umich.edu>
2018-07-11 19:48:38 +0800

    Add files via upload

--- Classification.py
+++ Classification.py
@@ -11,7 +11,11 @@ import seaborn as sns
 from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
 from sklearn import svm
+from sklearn import tree
+from sklearn import ensemble
 from keras import backend as K
+from sklearn.tree import export_graphviz
+import pydot
 
 def Error(y_pred, y_real):
     y_pred = np.nan_to_num(y_pred, copy = True)
@@ -25,9 +29,15 @@ def customLoss(y_pred, y_real):
     
 #------------------------------------------------------------------------------
 # Read in Data
-sensor_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\01_M01_DC_train.csv')
-faults_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\train_faults\\01_M01_train_fault_data.csv')
-ttf_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\train_ttf\\01_M01_DC_train.csv')
+# =============================================================================
+# sensor_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\01_M01_DC_train.csv')
+# faults_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\train_faults\\01_M01_train_fault_data.csv')
+# ttf_data = pd.read_csv('C:\\Users\\chen.zc\\Desktop\\phm_data_challenge_2018.tar\\train\\train_ttf\\01_M01_DC_train.csv')
+# 
+# =============================================================================
+sensor_data = pd.read_pickle('/home/ninja1mmm/Desktop/phm/data/train/01_M01_DC_train')
+faults_data = pd.read_pickle('/home/ninja1mmm/Desktop/phm/data/fault/01_M01_train_fault_data')
+ttf_data = pd.read_pickle('/home/ninja1mmm/Desktop/phm/data/ttf/01_M01_DC_train_ttf')
 
 sensor_data = sensor_data.drop(['Tool'], axis = 1)
 sensor_data = sensor_data.drop(['Lot'], axis = 1)
@@ -60,7 +70,7 @@ sensor_fault1, ttf_fault1, faults_fault1 = cutoff(sensor_data, faults_data, \
                     ttf_data, 'FlowCool Pressure Dropped Below Limit')    
 
 sensor_fault1 = sensor_fault1.fillna(method = 'ffill')
-sensor_fault1['recipe'] = sensor_fault1['recipe'] + 200
+#sensor_fault1['recipe'] = sensor_fault1['recipe'] + 200
 label = ttf_fault1['TTF_FlowCool Pressure Dropped Below Limit']
 
 #------------------------------------------------------------------------------
@@ -114,6 +124,8 @@ y_head = pd.DataFrame(y_head)
 y_tail = pd.DataFrame(y_tail)
 y = y_head.append(y_tail)
 df = df.drop(['time','runnum'], axis = 1)
+#df = df.drop(['time','runnum','ETCHSOURCEUSAGE','ETCHAUXSOURCETIMER','ETCHAUX2SOURCETIMER'], axis = 1)
+
 df = df.reset_index(drop = True)
 y = y.reset_index(drop = True)
 df['label'] = y
@@ -121,13 +133,49 @@ df['label'] = y
 # Normalization & split dataset
 df_scaler = preprocessing.StandardScaler()
 feature = df_scaler.fit_transform(df)
-X_train, X_valid = feature[:8000], feature[8000:]
-df = df.sample(frac=1).reset_index(drop=True)
-X_train, X_valid = df.iloc[:8000, :-1], df.iloc[8000:, :-1]
-y_train, y_valid = df.iloc[:8000, -1], df.iloc[8000:, -1]
+#X_train, X_valid = feature[:8000], feature[8000:]
+df_new = df.sample(frac=1).reset_index(drop=True)
+X_train, X_valid = df_new.iloc[:10000, :-1], df_new.iloc[10000:, :-1]
+y_train, y_valid = df_new.iloc[:10000, -1], df_new.iloc[10000:, -1]
 #------------------------------------------------------------------------------
 # SVM
-clf = svm.SVC(C = 1e3, gamma = 1)
+#clf = svm.SVC(C = 1, gamma = 1)
+# Decision Tree
+#clf = tree.DecisionTreeClassifier()
+#clf = tree.DecisionTreeClassifier(criterion = 'entropy')
+clf = ensemble.RandomForestClassifier(criterion = 'entropy')
+
 clf.fit(X_train, y_train)
 print('Training error is %f' %clf.score(X_train, y_train))
 print('Test error is %f' %clf.score(X_valid, y_valid))
+
+
+
+n = 2000
+X_pred,_ = Select_tail(sensor_fault1, label, trend_start_time, n)
+X_pred = X_pred.drop(['time','runnum'], axis = 1)
+y_pred = clf.predict(X_pred)
+print('Predicition error: %f' %(np.sum(y_pred)/len(y_pred)))
+
+plt.figure()
+for i in range(1,len(trend_start_time)):
+    plt.subplot(4,3,i)        
+    test_fault_period = sensor_fault1.iloc[trend_start_time[i-1]:trend_start_time[i],:]
+    test_pred = test_fault_period.drop(['time','runnum'], axis = 1)
+    test_y_pred = clf.predict(test_pred)
+    plt.plot(test_y_pred[-2000:])
+#    plt.plot(test_pred['recipe'])
+
+# Write dot file. Can be translated to pdf with line commands
+#with open("Fault1.dot", "w") as f:
+#    f = tree.export_graphviz(clf, out_file=f)
+
+
+# View the graph directly
+tree = clf.estimators_[5]
+# Export the image to a dot file
+export_graphviz(tree, out_file = 'tree.dot', feature_names = X_train.columns, rounded = True, precision = 1)
+# Use dot file to create a graph
+(graph, ) = pydot.graph_from_dot_file('tree.dot')
+# Write graph to a png file
+graph.write_png('tree.png')
